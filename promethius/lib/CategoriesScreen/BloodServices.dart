4ce4db42a4b427cbd3e61917ba88c5ce32a662e6
Suyoscar11 <suyoscar11@gmail.com>
2022-09-02 12:45:33 +0545

    Updated FrontEnd

---
+++ promethius/lib/CategoriesScreen/BloodServices.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BloodServices extends StatefulWidget {
+  const BloodServices({super.key});
+
+  @override
+  State<BloodServices> createState() => _BloodServicesState();
+}
+
+class _BloodServicesState extends State<BloodServices> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ promethius/lib/components/MedicinePurchase.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MedicinePurchase extends StatelessWidget {
+  const MedicinePurchase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
