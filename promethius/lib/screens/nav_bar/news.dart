2bbcc6d2bcf74254f709fc962bc19f514f44eaa4
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-09-02 21:38:24 +0545

    API fetch trial

--- promethius/lib/screens/nav_bar/news.dart
+++ promethius/lib/screens/nav_bar/news.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class NewsScreen extends StatefulWidget {
   const NewsScreen({Key? key}) : super(key: key);
 
@@ -7,8 +9,15 @@ class NewsScreen extends StatefulWidget {
 }
 
 class _NewsScreenState extends State<NewsScreen> {
+  String getNews() async {
+      http.Response res = await http.get('https://jsonplaceholder.typicode.com/posts');
+      print(res.status_code);
+      return res.body ;
+  }
+
   @override
   Widget build(BuildContext context) {
+    String a = getNews();
     return Container();
   }
 }
