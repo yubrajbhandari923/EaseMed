fe1129b069bf995c3b6ca37d3f75229869554a3a
n3pg0r <bishalkharel0000@gmail.com>
2022-09-03 07:21:21 +0545

    [Add] chatbot for our system and made endpoints fro each service

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import requests
 from fastapi import FastAPI
 from backend_code.health_news.health_news import news
+import json
+
+with open("backend_code/doctors_list/UpdatingDoctors.json") as s:
+    doctors_list=s.read()
+    doctors_list=json.loads(doctors_list)
+with open("backend_code/hospitals_list/hospitals_data.json") as s:
+    hospitals_data=s.read()
+    hospitals_data=json.loads(hospitals_data)
 
 app=FastAPI()
-
-
 
 @app.get("/news")
 async def get_news():
     data=news()
     return data
+
+@app.get("/doctors")
+async def get_doctors():
+    return doctors_list
+
+@app.get("/hospitals")
+async def get_hospitals():
+    return hospitals_data
+
